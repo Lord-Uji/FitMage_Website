abde72cee064ed098fb0b917d7de311db211bbf5
Lord-Uji <77410944+Lord-Uji@users.noreply.github.com>
2021-02-03 18:06:06 -0600

    Scroll/Btn/Nav Functionality

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 // Every JS file needs to start with $(document)
 
 $(document).ready(function(){
-
+/* For sticky navbar*/
     $('.js--section-features').waypoint(function(direction){
  
          if (direction == "down"){
@@ -16,4 +16,52 @@ $(document).ready(function(){
         }, {
             offset: '60px;'
     });
+
+    /*Scroll on buttons*/
+    /*--Hero Buttons--*/
+
+    $('.js--scroll-to-plan').click(function() {
+
+        $('html, body').animate({scrollTop: $('.js--section-signup').offset().top},1000);
+
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+        
+    });
+
+
+     /*--NAVBAR Buttons--*/
+    $('.js--btn-features').click(function() {
+
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+        
+    });
+
+    $('.js--btn-steps').click(function() {
+
+        $('html, body').animate({scrollTop: $('.js--section-steps').offset().top},1000);
+        
+    });
+
+    $('.js--btn-locations').click(function() {
+
+        $('html, body').animate({scrollTop: $('.js--section-cities').offset().top},1000);
+        
+    });
+
+    $('.js--btn-signup').click(function() {
+
+        $('html, body').animate({scrollTop: $('.js--section-signup').offset().top},1000);
+        
+    });
+
+    $('.js--btn-contact').click(function() {
+
+        $('html, body').animate({scrollTop: $('.js--section-contact').offset().top},1000);
+        
+    });
+
 });
