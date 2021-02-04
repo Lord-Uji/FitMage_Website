d806f32297258d5c138b70642253313e1d28a069
Lord-Uji <77410944+Lord-Uji@users.noreply.github.com>
2021-02-03 20:56:44 -0600

    jQuery functions (cont.)

--- resources/js/script.js
+++ resources/js/script.js
@@ -55,4 +55,31 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $('.js--section-contact').offset().top},1000);     
     });
 
+
+/*Animations on scroll*/
+
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    }, {
+        offset: '60%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset: '90%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    }, {
+        offset: '50%'
+    });
+   
 });
