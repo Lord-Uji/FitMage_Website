20f5ee5561af1ec723b4c8c136ad69285cd3dbd4
Lord-Uji <77410944+Lord-Uji@users.noreply.github.com>
2021-02-04 23:05:18 -0600

    jQuery functionality (cont.)

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animate__animated animate__fadeIn');
     }, {
-        offset: '50%'
+        offset: '70%'
     });
 
     $('.js--wp-2').waypoint(function(direction) {
@@ -81,5 +81,49 @@ $(document).ready(function(){
     }, {
         offset: '50%'
     });
-   
+
+    /*-- Mobile navigation--*/
+    $('.js--nav-icon, .js--main-nav a, .logo-text').click(function(element){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        //Gets the class name of the element that triggered the event
+        var clicked = element.target.className;
+
+        //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-text')
+            return;
+
+        //Opens and closes the menu
+        if ($(window).width() < 767){
+            nav.slideToggle(200);
+        }
+
+        //Changes icon states of the menu button
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 750){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });
+
 });
